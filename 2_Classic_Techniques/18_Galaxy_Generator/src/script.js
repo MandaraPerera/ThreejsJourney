ee7cb7fd6c06ebadba8de9df3e3ccb98e8df44e7
Mandara <mandara.kudaliyanagedonperera@student.kdg.be>
2025-01-31 16:28:05 +0100

    18_Galaxy_Generator

--- 2_Classic_Techniques/18_Galaxy_Generator/src/script.js
+++ 2_Classic_Techniques/18_Galaxy_Generator/src/script.js
@@ -3,6 +3,7 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 import Buffer from "three/addons/renderers/common/Buffer.js";
 import {BufferGeometry} from "three";
+import {Timer} from "three/addons";
 
 /**
  * Base
@@ -101,15 +102,16 @@ const generateGalaxy = () => {
 
 generateGalaxy()
 
-gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
-gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
-gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
-gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
+const galaxyTweaks = gui.addFolder('Galaxy')
+galaxyTweaks.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
+galaxyTweaks.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+galaxyTweaks.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+galaxyTweaks.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+galaxyTweaks.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
+galaxyTweaks.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
+galaxyTweaks.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
+galaxyTweaks.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+galaxyTweaks.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
 
 /**
  * Sizes
@@ -146,6 +148,14 @@ scene.add(camera)
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
+controls.autoRotate = true
+const controlsParameters = {
+    autoRotateSpeed: 2
+}
+
+const controlTweaks = gui.addFolder('Camera')
+controlTweaks.add(controls, 'autoRotate')
+controlTweaks.add(controlsParameters, 'autoRotateSpeed').min(1).max(1000).step(0.1)
 
 /**
  * Renderer
@@ -159,13 +169,14 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const timer = new Timer()
 
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
+    timer.update()
 
     // Update controls
     controls.update()
+    controls.autoRotateSpeed = controlsParameters.autoRotateSpeed * timer.getDelta() * 60
 
     // Render
     renderer.render(scene, camera)
